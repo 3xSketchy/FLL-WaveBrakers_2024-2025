91589cf8cb7332e951083ade92483074d4ee2df7
Sketchy <72786740+3xSketchy@users.noreply.github.com>
2024-10-12 22:08:01 +0300

    Change

--- Main Code/main.py
+++ Main Code/main.py
@@ -2,10 +2,8 @@
 
 
 from robot import Robot
-from pybricks.ev3devices import (Motor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button
 from pybricks.tools import wait, StopWatch
-from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile, Font
 from micropython import opt_level
 from pybricks.experimental import run_parallel
@@ -13,14 +11,14 @@ opt_level(2)
 
 robot = Robot()
 
-B_PID(40, 30)
-turn(30)
-B_PID(40, 15)
-turn(-30)
-B_PID(40, 20)
-turn(-60)
+robot.B_PID(40, 30)
+robotturn(30)
+robot.B_PID(40, 15)
+robot.turn(-30)
+robot.B_PID(40, 20)
+robot.turn(-60)
 B_PID(40, 12)
-PID(40, 14)
-turn(-30)
-B_PID(40,5)
-run_parallel(C_motor.run_angle(speed=180, rotation_angle=360), D_motor.run_angle(speed=180, rotation_angle=360))
+robot.PID(40, 14)
+robot.turn(-30)
+robot.B_PID(40,5)
+run_parallel(robot.C_motor.run_angle(speed=180, rotation_angle=360), robot.D_motor.run_angle(speed=180, rotation_angle=360))
