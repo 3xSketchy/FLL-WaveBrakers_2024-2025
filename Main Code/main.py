ac9d21323c7506d7300de2f8a42e9fbbee7d6bdd
Sketchy <72786740+3xSketchy@users.noreply.github.com>
2024-10-14 01:10:56 +0300

    Change

--- Main Code/main.py
+++ Main Code/main.py
@@ -10,15 +10,20 @@ from pybricks.experimental import run_parallel
 opt_level(2)
 
 robot = Robot()
+        
+def Run1():
+    KP = 2.25 #Proportional gain
+    KI = 0.00001 #Integral gain
+    KD = 3.5 #Derivative gain
 
-robot.B_PID(40, 30)
-robotturn(30)
-robot.B_PID(40, 15)
-robot.turn(-30)
-robot.B_PID(40, 20)
-robot.turn(-60)
-B_PID(40, 12)
-robot.PID(40, 14)
-robot.turn(-30)
-robot.B_PID(40,5)
-run_parallel(robot.C_motor.run_angle(speed=180, rotation_angle=360), robot.D_motor.run_angle(speed=180, rotation_angle=360))
+    robot.B_PID(40, 30, KP, KI, KD)
+    robot.turn(30, KP, KI, KD)
+    robot.B_PID(40, 15, KP, KI, KD)
+    robot.turn(-30, KP, KI, KD)
+    robot.B_PID(40, 20, KP, KI, KD)
+    robot.turn(-60, KP, KI, KD)
+    robot.B_PID(40, 12, KP, KI, KD)
+    robot.PID(40, 14, KP, KI, KD)
+    robot.turn(-30, KP, KI, KD)
+    robot.B_PID(40,5, KP, KI, KD)
+    run_parallel(robot.C_motor.run_angle(speed=180, rotation_angle=360), robot.D_motor.run_angle(speed=180, rotation_angle=360))
